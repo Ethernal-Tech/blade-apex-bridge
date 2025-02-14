8e20e8f57511a949213e869c1fd91d6c451b307c
oliverbundalo <oliver.bundalo@ethernal.tech>
2025-02-14 14:38:26 +0100

    pebble bench CI times fine tunning

--- blockchain/storagev2/pebble/pebble_perf_test.go
+++ blockchain/storagev2/pebble/pebble_perf_test.go
@@ -38,7 +38,7 @@ func Benchmark(b *testing.B) {
 	}()
 
 	blockCount := 1000
-	storagev2.BenchmarkStorage(b, blockCount, s, 60, 26) // CI times
+	storagev2.BenchmarkStorage(b, blockCount, s, 44, 24) // CI times
 
 	size, err := dbSize(path)
 	require.NoError(b, err)
