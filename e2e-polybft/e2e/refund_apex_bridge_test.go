3566f75b5ed0906f03dd414bc8fbb424e81ab54a
Dejan Nedic <dejan.nedic@ethernal.tech>
2025-04-17 10:51:52 +0200

    Fix

--- e2e-polybft/e2e/refund_apex_bridge_test.go
+++ e2e-polybft/e2e/refund_apex_bridge_test.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/cardanofw"
-	"github.com/0xPolygon/polygon-edge/e2e-polybft/e2ehelper"
 	infrawallet "github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 	"github.com/stretchr/testify/require"
 )
@@ -387,90 +386,4 @@ func TestE2E_Apex_Bridge_Refund_BatchRecreated(t *testing.T) {
 	require.NoError(t, err)
 
 	fmt.Println("newAmountDfm", newAmountDfm)
-}
-
-func TestE2E_Apex_Bridge_Refund_ComplexScenarios(t *testing.T) {
-	const (
-		apiKey  = "test_api_key"
-		userCnt = 10
-	)
-
-	ctx, cncl := context.WithCancel(context.Background())
-	defer cncl()
-
-	primeConfig, vectorConfig := cardanofw.NewPrimeChainConfig(), cardanofw.NewVectorChainConfig(true)
-	primeConfig.PremineAmount = 100_700_000_000
-	vectorConfig.PremineAmount = 500_000_000
-
-	apex := cardanofw.SetupAndRunApexBridge(
-		t, ctx,
-		cardanofw.WithAPIKey(apiKey),
-		cardanofw.WithUserCnt(userCnt),
-		cardanofw.WithPrimeConfig(primeConfig),
-		cardanofw.WithVectorConfig(vectorConfig),
-		cardanofw.WithCustomConfigHandlers(func(_ *cardanofw.ApexSystem, mp map[string]interface{}) {
-			mp["refundEnabled"] = true
-			tryCountLimitsSettings := cardanofw.GetMapFromInterfaceKey(mp, "tryCountLimits")
-			tryCountLimitsSettings["maxBatchTryCount"] = 2
-			tryCountLimitsSettings["maxSubmitTryCount"] = 2
-		}, nil),
-	)
-
-	defer require.True(t, apex.ApexBridgeProcessesRunning())
-
-	user := apex.Users[0]
-
-	txProviderPrime, err := apex.PrimeInfo.GetTxProvider()
-	require.NoError(t, err)
-
-	t.Run("From prime to vector - not enough funds on destination multisig address", func(t *testing.T) {
-		if cardanofw.ShouldSkipE2RRedundantTests() {
-			t.Skip()
-		}
-
-		const (
-			sendAmount  = uint64(100_600_000_000)
-			sendAmount2 = uint64(1_000_000)
-			feeAmount   = uint64(1_100_000)
-			instances   = 5
-		)
-
-		beforeSendingAmountDfm, err := apex.GetBalance(ctx, user, cardanofw.ChainIDPrime)
-		require.NoError(t, err)
-
-		receivers := map[string]uint64{
-			user.GetAddress(cardanofw.ChainIDVector): sendAmount,
-		}
-
-		bridgingRequestMetadata, err := cardanofw.CreateCardanoBridgingMetaData(
-			user.GetAddress(cardanofw.ChainIDPrime), receivers,
-			cardanofw.ChainIDVector, feeAmount)
-		require.NoError(t, err)
-
-		txHash, err := cardanofw.SendTx(
-			ctx, txProviderPrime, user.PrimeWallet, sendAmount+feeAmount, apex.PrimeInfo.MultisigAddr,
-			apex.Config.PrimeConfig.NetworkType, bridgingRequestMetadata)
-		require.NoError(t, err)
-
-		lowerBoundaryDfm := new(big.Int).Sub(beforeSendingAmountDfm, new(big.Int).SetUint64(sendAmount+feeAmount))
-
-		fmt.Printf("Tx sent. hash: %s, lowerBoundaryDfm: %d, higherBoundaryDfm: %d\n", txHash, lowerBoundaryDfm, beforeSendingAmountDfm)
-
-		e2ehelper.ExecuteBridging(
-			t, ctx, apex, 1, apex.Users[1:instances+1], []*cardanofw.TestApexUser{user},
-			[]string{cardanofw.ChainIDPrime},
-			map[string][]string{
-				cardanofw.ChainIDPrime: {cardanofw.ChainIDVector},
-			}, new(big.Int).SetUint64(sendAmount2))
-
-		err = apex.WaitForAmountInRange(ctx, user, cardanofw.ChainIDPrime, lowerBoundaryDfm, beforeSendingAmountDfm,
-			20, time.Second*30)
-		require.NoError(t, err)
-
-		newAmountDfm, err := apex.GetBalance(ctx, user, cardanofw.ChainIDPrime)
-		require.NoError(t, err)
-
-		fmt.Println("newAmountDfm", newAmountDfm)
-	})
-
 }
