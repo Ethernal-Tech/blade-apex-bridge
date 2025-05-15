7ff5e8ca8eff91f6eda9a020b3247c095ac83640
Dejan-Nedic <dejan.nedic@ethernal.tech>
2025-05-15 20:06:01 +0200

    AD-643 E2E Big tests (#67)
    * Split E2E tests job
    
    * Refactor big tests. New big test for all directions.
    
    * Revert worklow, remove nexus tests.
    
    * Fix
    
    * Fix

--- e2e-polybft/cardanofw/test-apex-system.go
+++ e2e-polybft/cardanofw/test-apex-system.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"strings"
 	"sync"
 	"testing"
 	"time"
@@ -546,6 +547,11 @@ func (a *ApexSystem) SubmitBridgingRequest(
 ) string {
 	t.Helper()
 
+	const (
+		numRetries = 5
+		waitTime   = time.Second * 10
+	)
+
 	require.True(t, sourceChain != destinationChain)
 
 	// check if sourceChain is supported
@@ -597,8 +603,20 @@ func (a *ApexSystem) SubmitBridgingRequest(
 	privateKey, err := sender.GetPrivateKey(sourceChain)
 	require.NoError(t, err)
 
-	txHash, err := a.GetChainMust(t, sourceChain).BridgingRequest(
-		ctx, destinationChain, privateKey, receiversMap, feeAmount)
+	txHash, err := infracommon.ExecuteWithRetry(ctx, func(ctx context.Context) (string, error) {
+		txHash, err := a.GetChainMust(t, sourceChain).BridgingRequest(
+			ctx, destinationChain, privateKey, receiversMap, feeAmount)
+		if err != nil {
+			if strings.Contains(err.Error(), "The transaction contains unknown UTxO references as inputs") ||
+				strings.Contains(err.Error(), infracommon.ErrRetryTimeout.Error()) {
+				return "", infracommon.ErrRetryTryAgain
+			}
+
+			return "", err
+		}
+
+		return txHash, nil
+	}, infracommon.WithRetryCount(numRetries), infracommon.WithRetryWaitTime(waitTime))
 	require.NoError(t, err)
 
 	return txHash
