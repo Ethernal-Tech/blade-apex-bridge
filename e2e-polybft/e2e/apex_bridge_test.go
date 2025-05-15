eba8b393bbc2d3b5ad223d9b477d12179fcc0152
Igor Crevar <crewce@gmail.com>
2025-05-15 13:28:28 +0200

    Revert "revert this later"
    This reverts commit 0daf5c41f4bfde3134519f4994be83f77ea128f0.

--- e2e-polybft/e2e/apex_bridge_test.go
+++ e2e-polybft/e2e/apex_bridge_test.go
@@ -130,6 +130,10 @@ func Test_OnlyRunApexBridge_WithNexusAndVector(t *testing.T) {
 }
 
 func TestE2E_ApexBridge_UpdateApexBridgeSmartContract(t *testing.T) {
+	if cardanofw.ShouldSkipE2RRedundantTests() {
+		t.Skip()
+	}
+
 	currentWorkingDir, err := os.Getwd()
 	require.NoError(t, err)
 
