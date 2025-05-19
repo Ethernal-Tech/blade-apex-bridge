d7a7a810c95a2ccc55a4056b87c75faf778e770f
Dejan Nedic <dejan.nedic@ethernal.tech>
2025-05-19 14:49:06 +0200

    Fix

--- e2e-polybft/e2e/apex_bridge_test.go
+++ e2e-polybft/e2e/apex_bridge_test.go
@@ -1624,7 +1624,7 @@ func TestE2E_ApexBridge_ValidScenarios_BigTests_AllDirections(t *testing.T) {
 						time.Sleep(time.Second * time.Duration(r.Intn(maxWaitTime)))
 
 						apex.SubmitBridgingRequest(t, ctx, src, dest, apex.Users[idx], sendAmount, sendtx.BridgingTypeNormal, user)
-					} else {
+					} else if src != cardanofw.ChainIDNexus {
 						submitInvalidSendAmountTransaction(t, ctx, apex, src, dest, apex.Users[idx], sendAmount, user.GetAddress(dest))
 					}
 				}(br.firstSenderIdx+i, j, br.src, br.dest, success)
@@ -1865,12 +1865,7 @@ func submitInvalidSendAmountTransaction(
 
 	_, err = apex.SubmitTx(ctx, src, senderUser, apex.GetChainMust(t, src).GetHotWalletAddress(),
 		new(big.Int).Add(sendAmount, feeAmount), nil, bridgingRequestMetadata)
-
-	if src == cardanofw.ChainIDNexus {
-		require.ErrorContains(t, err, "fund relayer failed") // ReceiptFailed status
-	} else {
-		require.NoError(t, err)
-	}
+	require.NoError(t, err)
 }
 
 func PrimeToVectorInvalidMetadataSlicedOff(
