b98de3c7e9607cd959a9bf5209a841f0e4bf8371
Dejan Nedic <dejan.nedic@ethernal.tech>
2025-07-31 13:42:37 +0200

    Fix min utxo valur for defund when there are tokens in utxos

--- e2e-polybft/e2e/testnet_bridge_test.go
+++ e2e-polybft/e2e/testnet_bridge_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/cardanofw"
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/e2ehelper"
 	infracommon "github.com/Ethernal-Tech/cardano-infrastructure/common"
+	cardanowallet "github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 	"github.com/Ethernal-Tech/ethgo"
 	"github.com/stretchr/testify/require"
 )
@@ -86,6 +87,11 @@ func Test_E2E_TestnetDefund(t *testing.T) {
 
 	chains := getEnabledChains(apex)
 
+	info := map[string]*cardanofw.CardanoChainInfo{
+		cardanofw.ChainIDPrime:  &apex.PrimeInfo,
+		cardanofw.ChainIDVector: &apex.VectorInfo,
+	}
+
 	for _, user := range apex.Users {
 		for _, chain := range chains {
 			addr := user.GetAddress(chain)
@@ -99,7 +105,32 @@ func Test_E2E_TestnetDefund(t *testing.T) {
 				change = new(big.Int).SetUint64(cardanofw.PotentialFee)
 				balanceAtleast = new(big.Int).Set(change)
 			} else {
-				change = new(big.Int).SetUint64(cardanofw.MinUTxODefaultValue + cardanofw.PotentialFee)
+				txProvider, err := info[chain].GetTxProvider()
+				require.NoError(t, err)
+
+				protParams, err := infracommon.ExecuteWithRetry(ctx, func(ctx context.Context) ([]byte, error) {
+					return txProvider.GetProtocolParameters(ctx)
+				})
+				require.NoError(t, err)
+
+				utxos, err := txProvider.GetUtxos(ctx, addr)
+				require.NoError(t, err)
+
+				balance := cardanowallet.GetUtxosSum(utxos)
+
+				tokens, err := cardanowallet.GetTokensFromSumMap(balance)
+				require.NoError(t, err)
+
+				txBuilder, err := getTxBuilder(apex, chain)
+				require.NoError(t, err)
+
+				minUtxo, err := txBuilder.SetProtocolParameters(protParams).CalculateMinUtxo(cardanowallet.TxOutput{
+					Addr:   addr,
+					Tokens: tokens,
+				})
+				require.NoError(t, err)
+
+				change = new(big.Int).SetUint64(max(minUtxo, cardanofw.MinUTxODefaultValue) + cardanofw.PotentialFee)
 				balanceAtleast = big.NewInt(0).Add(new(big.Int).SetUint64(cardanofw.MinUTxODefaultValue), change)
 			}
 
@@ -474,4 +505,15 @@ func getEnabledDirections(apex *cardanofw.ApexSystem) []BridgingRequest {
 	}
 
 	return directions
+}
+
+func getTxBuilder(apex *cardanofw.ApexSystem, chain string) (*cardanowallet.TxBuilder, error) {
+	switch chain {
+	case cardanofw.ChainIDPrime:
+		return cardanowallet.NewTxBuilder(cardanowallet.ResolveCardanoCliBinary(apex.Config.PrimeConfig.NetworkType))
+	case cardanofw.ChainIDVector:
+		return cardanowallet.NewTxBuilder(cardanowallet.ResolveCardanoCliBinary(apex.Config.VectorConfig.NetworkType))
+	default:
+		return nil, fmt.Errorf("unknown chain: %s", chain)
+	}
 }
