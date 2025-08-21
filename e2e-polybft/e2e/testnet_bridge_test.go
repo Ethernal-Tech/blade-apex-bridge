c7ac985d406be3fd9d0ada303bc05433d89c4135
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2025-08-21 10:11:14 +0200

    simplify IsVectorEnabled

--- e2e-polybft/e2e/testnet_bridge_test.go
+++ e2e-polybft/e2e/testnet_bridge_test.go
@@ -458,11 +458,7 @@ func getUserLovelaceBalances(
 }
 
 func IsVectorEnabled(apex *cardanofw.ApexSystem) bool {
-	if apex.Config.VectorConfig == nil {
-		return false
-	}
-
-	return apex.Config.VectorConfig.IsEnabled
+	return apex.Config.VectorConfig != nil && apex.Config.VectorConfig.IsEnabled
 }
 
 func getEnabledChains(apex *cardanofw.ApexSystem) []string {
