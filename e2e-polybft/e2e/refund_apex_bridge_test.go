baaa443fff213937b98f7bc6f82f444ace8fa696
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2025-09-19 12:58:20 +0200

    retry on ogmios call

--- e2e-polybft/e2e/refund_apex_bridge_test.go
+++ e2e-polybft/e2e/refund_apex_bridge_test.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/cardanofw"
 	"github.com/0xPolygon/polygon-edge/e2e-polybft/e2ehelper"
+	infracommon "github.com/Ethernal-Tech/cardano-infrastructure/common"
 	infrawallet "github.com/Ethernal-Tech/cardano-infrastructure/wallet"
 	"github.com/stretchr/testify/require"
 )
@@ -198,7 +199,11 @@ func TestE2E_ApexRefund_ValidScenarios(t *testing.T) {
 
 				testUser := apex.Users[idx]
 
-				beforeSendingAmountDfm, err := apex.GetBalance(ctx, testUser, cardanofw.ChainIDPrime)
+				beforeSendingAmountDfm, err := infracommon.ExecuteWithRetry(
+					ctx, func(ctx context.Context) (*big.Int, error) {
+						return apex.GetBalance(ctx, testUser, cardanofw.ChainIDPrime)
+					},
+				)
 				require.NoError(t, err)
 
 				lowerBoundaryDfm := new(big.Int).Sub(beforeSendingAmountDfm, new(big.Int).SetUint64(sendAmount+feeAmount))
