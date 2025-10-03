8e16a010b1a0be301424734e41f7972cbf0cffa9
Igor Crevar <crewce@gmail.com>
2025-10-03 11:53:49 +0200

    AD-624 Support for cardano-cli / node / ogmios 10.x.x

--- e2e-polybft/e2e/testnet_skyline_test.go
+++ e2e-polybft/e2e/testnet_skyline_test.go
@@ -135,6 +135,7 @@ func Test_E2E_SkylineTestnetDefund(t *testing.T) {
 
 			receiverMinUtxo, err := txBuilder.SetProtocolParameters(protParams).CalculateMinUtxo(cardanowallet.TxOutput{
 				Addr:   senderAddr.String(),
+				Amount: balance[cardanowallet.AdaTokenName] - cardanofw.MinUTxODefaultValue - cardanofw.PotentialFee,
 				Tokens: tokens,
 			})
 			require.NoError(t, err)
