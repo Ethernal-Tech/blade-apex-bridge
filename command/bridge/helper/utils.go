dfb43c060ef5edbf3496135f502368fd079b28d1
babinskizoltan <babinskizoltan@gmail.com>
2025-11-10 12:15:09 +0100

    remove printfs

--- command/bridge/helper/utils.go
+++ command/bridge/helper/utils.go
@@ -110,8 +110,6 @@ func GetPrivateKeyForCommand(key string) (crypto.Key, error) {
 		return nil, err
 	}
 
-	fmt.Printf("got secret for key: %s, string(secret): %s, hex.EncodeToString(secret): %s\n\n", val[1], string(privateKeySecretsManager), hex.EncodeToString(privateKeySecretsManager))
-
 	pkBytes, err := hex.DecodeHex(string(privateKeySecretsManager))
 	if err != nil {
 		return nil, err
@@ -121,13 +119,6 @@ func GetPrivateKeyForCommand(key string) (crypto.Key, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize key from provided config: %w", err)
 	}
-
-	privateKeyBytes, err := privateKey.MarshallPrivateKey()
-	if err != nil {
-		return nil, fmt.Errorf("failed to marshal privateKey: %w", err)
-	}
-
-	fmt.Printf("marshaling created crypto.ECDSAKey. string(privateKeyBytes): %s, hex.EncodeToString(privateKeyBytes): %s\n\n", string(privateKeyBytes), hex.EncodeToString(privateKeyBytes))
 
 	return privateKey, nil
 }
