44fa1e621276da5600bc4da5ced1d29f88fb3981
Zoltan Babinski <babinskizoltan@gmail.com>
2025-12-19 15:54:58 +0100

    testnet tests fix

--- e2e-polybft/e2e/testnet_skyline_test.go
+++ e2e-polybft/e2e/testnet_skyline_test.go
@@ -581,7 +581,7 @@ func TestE2E_SkylineTestnetBridge_ValidScenarios_ColoredCoins(t *testing.T) {
 		options := append([]e2ehelper.ExecuteBridgingOption{}, bridgingOpts...)
 		options = append(options, e2ehelper.WithColoredCoins([]uint16{cardanofw.USDTTokenID}))
 
-		for _, user := range apex.Users[:parallelInstances] {
+		for _, user := range apex.Users[len(apex.Users)-parallelInstances:] {
 			e2ehelper.ExecuteSingleBridging(
 				t, ctx, apex, user, user, cardanofw.ChainIDNexus, cardanofw.ChainIDVector, fundAmount,
 				cardanofw.BridgingTypeColoredCoinOnSource, options...)
@@ -598,7 +598,7 @@ func TestE2E_SkylineTestnetBridge_ValidScenarios_ColoredCoins(t *testing.T) {
 	t.Run("11. Nexus <-> Vector xADA both directions parallel", func(t *testing.T) {
 		fundAmount := new(big.Int).Mul(sendAmountDfm, big.NewInt(sequentialInstances*parallelInstances))
 
-		for _, user := range apex.Users[:parallelInstances] {
+		for _, user := range apex.Users[len(apex.Users)-parallelInstances:] {
 			e2ehelper.ExecuteSingleBridging(
 				t, ctx, apex, user, user, cardanofw.ChainIDCardano, cardanofw.ChainIDNexus, fundAmount,
 				cardanofw.BridgingTypeCurrencyOnSource)
@@ -615,7 +615,7 @@ func TestE2E_SkylineTestnetBridge_ValidScenarios_ColoredCoins(t *testing.T) {
 	t.Run("12. Nexus <-> Cardano xADA both directions parallel", func(t *testing.T) {
 		fundAmount := new(big.Int).Mul(sendAmountDfm, big.NewInt(sequentialInstances*parallelInstances))
 
-		for _, user := range apex.Users[:parallelInstances] {
+		for _, user := range apex.Users[len(apex.Users)-parallelInstances:] {
 			e2ehelper.ExecuteSingleBridging(
 				t, ctx, apex, user, user, cardanofw.ChainIDCardano, cardanofw.ChainIDNexus, fundAmount,
 				cardanofw.BridgingTypeCurrencyOnSource)
