04c0b72ee0c6813cac8de500e95501ab3d2d489c
milos-ethernal <milos.maksimovic@ethernal.tech>
2026-04-22 13:40:23 +0200

    fix failing tests

--- e2e-polybft/e2e/skyline_solana_test.go
+++ e2e-polybft/e2e/skyline_solana_test.go
@@ -519,6 +519,12 @@ func Test_SkylineSolana_ValidScenarios(t *testing.T) {
 			[]*cardanofw.TestApexUser{user},
 			getBridgingRequests(bridgingRequests),
 			bridgingAmount,
+			e2ehelper.WithTimeoutConfig(e2ehelper.NewTimeoutConfig(
+				e2ehelper.WithBridgingNumRetries(100),
+				e2ehelper.WithBridgingRetryWaitTime(20*time.Second),
+				e2ehelper.WithUnexpectedBridgesNumRetries(12),
+				e2ehelper.WithUnexpectedBridgesRetryWaitTime(10*time.Second),
+			)),
 		)
 	})
 
@@ -547,6 +553,12 @@ func Test_SkylineSolana_ValidScenarios(t *testing.T) {
 			[]*cardanofw.TestApexUser{user},
 			getBridgingRequests(bridgingRequests),
 			bridgingAmount,
+			e2ehelper.WithTimeoutConfig(e2ehelper.NewTimeoutConfig(
+				e2ehelper.WithBridgingNumRetries(100),
+				e2ehelper.WithBridgingRetryWaitTime(20*time.Second),
+				e2ehelper.WithUnexpectedBridgesNumRetries(12),
+				e2ehelper.WithUnexpectedBridgesRetryWaitTime(10*time.Second),
+			)),
 		)
 	})
 }
