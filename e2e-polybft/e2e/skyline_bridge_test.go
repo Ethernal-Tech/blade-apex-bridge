ebb7d7556c2c70e137d2c5bdc2dd8bfed100e547
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2026-06-29 09:45:19 +0200

    Add vector and nexus chain data in Test_OnlyRunSkylineBridge

--- e2e-polybft/e2e/skyline_bridge_test.go
+++ e2e-polybft/e2e/skyline_bridge_test.go
@@ -84,6 +84,13 @@ func Test_OnlyRunSkylineBridge(t *testing.T) {
 	fmt.Printf("prime treasury addr: %s\n", primeConfig.TreasuryAddress)
 	fmt.Printf("prime socket path: %s\n", apex.PrimeInfo.SocketPath)
 
+	fmt.Printf("vector network url: %s\n", apex.VectorInfo.NetworkAddress)
+	fmt.Printf("vector ogmios url: %s\n", apex.VectorInfo.OgmiosURL)
+	fmt.Printf("vector bridging addr: %s\n", apex.VectorInfo.MultisigAddr[0])
+	fmt.Printf("vector fee addr: %s\n", apex.VectorInfo.FeeAddr)
+	fmt.Printf("vector treasury addr: %s\n", vectorConfig.TreasuryAddress)
+	fmt.Printf("vector socket path: %s\n", apex.VectorInfo.SocketPath)
+
 	fmt.Printf("cardano network url: %s\n", apex.CardanoInfo.NetworkAddress)
 	fmt.Printf("cardano ogmios url: %s\n", apex.CardanoInfo.OgmiosURL)
 	fmt.Printf("cardano bridging addr: %s\n", apex.CardanoInfo.MultisigAddr[0])
@@ -103,6 +110,10 @@ func Test_OnlyRunSkylineBridge(t *testing.T) {
 	user := apex.Users[0]
 	userPrimeSK, err := user.GetPrivateKey(cardanofw.ChainIDPrime)
 	require.NoError(t, err)
+	userVectorSK, err := user.GetPrivateKey(cardanofw.ChainIDVector)
+	require.NoError(t, err)
+	userNexusSK, err := user.GetPrivateKey(cardanofw.ChainIDNexus)
+	require.NoError(t, err)
 	userCardanoSK, err := user.GetPrivateKey(cardanofw.ChainIDCardano)
 	require.NoError(t, err)
 
@@ -110,6 +121,10 @@ func Test_OnlyRunSkylineBridge(t *testing.T) {
 	fmt.Printf("user prime signing key hex: %s\n", userPrimeSK)
 	fmt.Printf("user cardano addr: %s\n", user.GetAddress(cardanofw.ChainIDCardano))
 	fmt.Printf("user cardano signing key hex: %s\n", userCardanoSK)
+	fmt.Printf("user vector addr: %s\n", user.GetAddress(cardanofw.ChainIDVector))
+	fmt.Printf("user vector signing key hex: %s\n", userVectorSK)
+	fmt.Printf("user nexus addr: %s\n", user.GetAddress(cardanofw.ChainIDNexus))
+	fmt.Printf("user nexus signing key hex: %s\n", userNexusSK)
 	fmt.Printf("user solana addr: %s\n", user.SolanaAddress)
 	fmt.Printf("user solana signing key hex: %s\n", user.SolanaWallet.PrivateKey.String())
 
