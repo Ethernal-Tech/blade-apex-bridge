c582ef38dcb0d18b3b5e61757fc4647c44683367
Nikola Jovićević <nikola.jovicevic@ethernal.tech>
2026-07-24 21:43:56 +0200

    fix retriable errors being treated as non-retriable

--- e2e-polybft/cardanofw/test-apex-system.go
+++ e2e-polybft/cardanofw/test-apex-system.go
@@ -2137,8 +2137,9 @@ func (a *ApexSystem) RedistributeTokens(
 }
 
 func IsRetryableSubmitTx(err error) bool {
-	// receipt polling failures must not trigger a full resubmit (tx may already be on-chain)
-	if errors.Is(err, txrelayer.ErrFailedToRetrieveTxReceipt) {
+	// receipt polling failures must not trigger a full resubmit (tx may already be on-chain),
+	// except in situations where the error is specified as retryable
+	if errors.Is(err, txrelayer.ErrFailedToRetrieveTxReceipt) && !infracommon.IsRetryableError(err) {
 		return false
 	}
 
